bf9e600145f47148364fe3285d7efb04ae32f3e7
Blessing Agadagba <agadagbablessing@gmail.com>
2021-09-24 16:27:32 +0300

    add doc strings

--- app/app.py
+++ app/app.py
@@ -7,11 +7,16 @@ import os
 
 
 def process_input(request_data: str) -> np.array:
+    '''
+    transforms JSON type data acquired from request and transforms it into 2D array the model understands
+    :param posted_data:
+    :return:np.array
+    '''
     parsed_body = np.asarray(json.loads(request_data)["features"])
     assert len(parsed_body.shape) == 2, "'inputs' must be a 2-d array"
     return parsed_body
 
-working_dir = os. getcwd()
+working_dir = os.getcwd()
 SAVED_MODEL_PATH = working_dir + "/app/models/model.pkl"
 
 model = pickle.load(open(SAVED_MODEL_PATH, "rb"))
@@ -24,6 +29,10 @@ def home() -> str:
 
 @app.route("/predict", methods=["POST"])
 def predict() -> str:
+     '''
+    loads the data acquired from request to the model and returns the predicted value
+    :return: prediction
+    '''
     try:
         input_params = process_input(request.data)
         predictions = model.predict(input_params)
